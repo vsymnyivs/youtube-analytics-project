a0ea5f420d22aabedeb72a929f67bd2fc0a7e2c2
vsymnyivs <vsymnyivs@gmail.com>
2024-08-08 23:45:35 +0300

    add hw16.1 complete

--- src/video.py
+++ src/video.py
@@ -11,12 +11,18 @@ class Video:
 
     def __init__(self, video_id: str) -> None:
         self.video_id = video_id
-        self.video_response = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                                         id=video_id).execute()
-        self.url = f"https://www.youtube.com/channel/{self.video_id}"
-        self.title = self.video_response["items"][0]["snippet"]["title"]
-        self.view_count = self.video_response["items"][0]["statistics"]["viewCount"]
-        self.like_count = self.video_response["items"][0]["statistics"]["likeCount"]
+        try:
+            self.video_response = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                             id=self.video_id).execute()
+            self.title = self.video_response['items'][0]['snippet']['title']
+            self.url = f'https://www.youtube.com/watch?v={self.video_id}'
+            self.view_count = self.video_response['items'][0]['statistics']['viewCount']
+            self.like_count = self.video_response['items'][0]['statistics']['likeCount']
+        except Exception:
+            self.title = None
+            self.url = None
+            self.view_count = None
+            self.like_count = None
 
     def __str__(self):
         """
